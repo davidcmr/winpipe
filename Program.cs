159a1d77d54cd16db77c3759abd3953f2a62331a
David Christie <david.christie@mediaradar.com>
2026-02-24 10:05:13 -0500

    Minimal audio recording using NAudio to create a wav file

--- Program.cs
+++ Program.cs
@@ -1,1 +1,29 @@
-﻿
+﻿using NAudio.Wave;
+
+var outputFolder = Path.Combine(Directory.GetCurrentDirectory(), ".winpipe-audio");
+Directory.CreateDirectory(outputFolder);
+var outputFilePath = Path.Combine(outputFolder, $"recorded-{DateTime.Now:yyyyMMdd_HHmmss}.wav");
+var capture = new WasapiLoopbackCapture();
+var writer = new WaveFileWriter(outputFilePath, capture.WaveFormat);
+
+capture.DataAvailable += (s, a) =>
+{
+    writer.Write(a.Buffer, 0, a.BytesRecorded);
+    if (writer.Position > capture.WaveFormat.AverageBytesPerSecond * 20)
+    {
+        capture.StopRecording();
+    }
+};
+
+capture.RecordingStopped += (s, a) =>
+{
+    writer.Dispose();
+    writer = null;
+    capture.Dispose();
+};
+
+capture.StartRecording();
+while (capture.CaptureState != NAudio.CoreAudioApi.CaptureState.Stopped)
+{
+    Thread.Sleep(500);
+}
